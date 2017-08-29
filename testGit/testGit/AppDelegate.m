7ad26b5f89ba71cbc2222ee89d86f4042469d996
leoChan <leoChan@leoChanProdeMacBook-Pro.local>
2017-08-29 11:43:34 +0800

    更改了一下appdelegate

--- testGit/testGit/AppDelegate.m
+++ testGit/testGit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"2121212121212121");
     return YES;
 }
 
